d8d8882951d899f67f20b9fd099a62cb73f37def
Himanshu-Negi8 <himanshunegi3295@gmail.com>
2025-01-17 13:29:25 +0530

    there is something more in server.go than what you see

--- server/server.go
+++ server/server.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"errors"
 	"fmt"
-	"github.com/Himanshu-Negi8/build-your-own-redis-server/handler"
-	"github.com/Himanshu-Negi8/build-your-own-redis-server/parser"
-	"github.com/Himanshu-Negi8/build-your-own-redis-server/types"
 	"io"
 	"net"
 	"os"
+
+	"github.com/Himanshu-Negi8/build-your-own-redis-server/handler"
+	"github.com/Himanshu-Negi8/build-your-own-redis-server/parser"
+	"github.com/Himanshu-Negi8/build-your-own-redis-server/types"
 )
+
+const workers = 3
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
@@ -21,30 +25,55 @@ func main() {
 	defer l.Close()
 
 	cache := make(map[string]types.CustomValue)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-	for {
-
-		conn, err := l.Accept()
-		if err != nil {
-			fmt.Println("failed to serve requests")
-			continue
-		}
-
-		go handlePingRequests(conn, cache)
+	connChan := make(chan net.Conn)
+	for i := 1; i <= workers; i++ {
+		go worker(ctx, connChan, cache)
 	}
 
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			conn, err := l.Accept()
+			if err != nil {
+				fmt.Println("failed to serve requests")
+				continue
+			}
+			connChan <- conn
+		}
+	}
 }
 
-func handlePingRequests(conn net.Conn, cache map[string]types.CustomValue) {
-	defer conn.Close()
+// creating a worker pool which will have a ctx when signaled it will return
+func worker(ctx context.Context, connCh chan net.Conn, cache map[string]types.CustomValue) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case conn, ok := <-connCh:
+			if !ok {
+				// Channel closed, exit the worker
+				return
+			}
+			handleConnectionRequest(conn, cache)
+		}
+	}
+}
 
+func handleConnectionRequest(conn net.Conn, cache map[string]types.CustomValue) {
+	defer conn.Close()
 	buf := make([]byte, 2048)
+
 	for {
 		// Read the client's input
 		_, err := conn.Read(buf)
 		if err != nil {
 			if errors.Is(err, io.EOF) {
-				// Client closed the connection
+				// Client closed the connection.
 				fmt.Println("Client disconnected")
 				return
 			}
